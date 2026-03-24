28efd2124fb0c8b014b911149a59db967ddf651b
Riordan Pawley <riordan@chefy.com.au>
2026-03-24 14:25:53 +1100

    afj: scaffold daemon and client package skeleton roots

---
+++ go-bubbletea/internal/client/client.go
@@ -0,0 +1,9 @@
+package client
+
+// Client is the root client package handle.
+type Client struct{}
+
+// New returns a placeholder client instance.
+func New() *Client {
+	return &Client{}
+}
---
+++ go-bubbletea/internal/daemon/daemon.go
@@ -0,0 +1,9 @@
+package daemon
+
+// Daemon is the root daemon package handle.
+type Daemon struct{}
+
+// New returns a placeholder daemon instance.
+func New() *Daemon {
+	return &Daemon{}
+}
