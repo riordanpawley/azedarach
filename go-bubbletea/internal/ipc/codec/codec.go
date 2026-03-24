497cf18158ef93b71ba6e43ed2613d7028bbb832
Riordan Pawley <riordan@chefy.com.au>
2026-03-24 14:19:59 +1100

    afi: scaffold ipc transport and codec package skeleton

---
+++ go-bubbletea/internal/ipc/codec/codec.go
@@ -0,0 +1,9 @@
+package codec
+
+// Codec is the IPC codec placeholder.
+type Codec struct{}
+
+// NewCodec returns a codec placeholder.
+func NewCodec() *Codec {
+	return &Codec{}
+}
---
+++ go-bubbletea/internal/ipc/transport/transport.go
@@ -0,0 +1,9 @@
+package transport
+
+// Transport is the IPC transport placeholder.
+type Transport struct{}
+
+// NewTransport returns a transport placeholder.
+func NewTransport() *Transport {
+	return &Transport{}
+}
